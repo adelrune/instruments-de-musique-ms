82229157fa476eefa948ad2d1bfeb101781be7a7
adelrune <guiloua2@hotmail.com>
2020-09-16 09:11:41 -0400

    adds a visualisation to the coord paster

--- scripts/tell_me_the_coord.js
+++ scripts/tell_me_the_coord.js
@@ -1,9 +1,11 @@
 
 var pos_rect = {sound:"", link:""};
 
+visualiser = null;
+
 document.addEventListener("click", function(e) {
     var image = e.target;
-    image_rect = image.getBoundingClientRect();
+    var image_rect = image.getBoundingClientRect();
     if (pos_rect.x1 === undefined) {
         pos_rect.x1 = e.clientX - image_rect.x;
         pos_rect.y1 = e.clientY - image_rect.y;
@@ -13,6 +15,44 @@ document.addEventListener("click", function(e) {
         console.log(pos_rect);
         navigator.clipboard.writeText(JSON.stringify(pos_rect));
         pos_rect = {sound:"", link:""};
+
+        (visualiser != null) && (visualiser.style.display = "none");
     }
     return true;
+});
+
+vis_mode = false;
+
+document.addEventListener("keydown", function(e) {
+    if (event.code == "Escape") {
+        console.log("escape");
+        pos_rect = {sound:"", link:""};
+        (visualiser != null) && (visualiser.style.display = "none");
+    }
+    if (event.code != "Digit1" || vis_mode) {
+        return;
+    }
+    visualiser = document.createElement("div");
+    visualiser.style.display = "none";
+    document.body.appendChild(visualiser);
+    vis_mode = true;
+
+    document.addEventListener("mousemove", function(e) {
+        if(pos_rect.x1 === undefined) {
+            return;
+        }
+        var image_rect = image.getBoundingClientRect();
+        visualiser.style = {};
+        visualiser.style.display = "block";
+        visualiser.style.backgroundColor = "rgb(0, 239, 232)";
+        visualiser.style.position = "absolute";
+        visualiser.style.opacity = "24%";
+        visualiser.style.zIndex = 9001;
+        visualiser.style.width = e.clientX - (image_rect.x + pos_rect.x1) + "px";
+        visualiser.style.height = e.clientY - (image_rect.y + pos_rect.y1) + "px";
+        visualiser.style.left = image_rect.x + pos_rect.x1 + "px";
+        visualiser.style.top = image_rect.y + pos_rect.y1 + "px";
+
+    });
+
 });
